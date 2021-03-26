ed3fbfc2379149aab336f5d7e7632caf74a67ce8
jim <jbmbroomfield@gmail.com>
2021-03-26 20:13:49 +1100

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+clickAlert = () => {
+    alert('I was clicked!')
 }
+
+function addingEventListener() {
+    const input = document.getElementById('input')
+    input.addEventListener('click', clickAlert)
+
+    const div5 = document.getElementById('div5')
+    div5.addEventListener('click',clickAlert)
+}
+
+addingEventListener()
